be913184d3d0fe0c9cefe32eb5ca9b8d9d9acafa
olgamartinchik <sintcerova@mail.ru>
2021-11-04 15:38:12 +0300

    feat: add 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,5 +1,29 @@
 const fs = require("fs");
 const path = require("path");
-const link = path.resolve("./secret-folder/");
+const link = path.join(__dirname, "secret-folder");
 
 console.log(link);
+
+fs.readdir(link, { withFileTypes: true }, (err, files) => {
+  console.log("\nCurrent directory files:");
+  if (err) console.log(err);
+  else {
+    files.forEach((file) => {
+      if (!file.isDirectory()) {
+        fs.stat(path.join(link, file.name), (err, states) => {
+          if (err) {
+            console.log(err);
+          } else {
+            // console.log(states.size);
+            console.log(
+              `${path.parse(file.name).name} - ${path
+                .parse(file.name)
+                .ext.replace(".", "")} - ${states.size}`
+            );
+          }
+        });
+        // console.log(file.name);
+      }
+    });
+  }
+});
